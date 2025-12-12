34916bd47a967fedb2767ded2cc084f93d5f9e06
Yeasin <ar9485727@gmail.com>
2025-12-12 20:13:12 +0600

    some changes

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,11 +1,9 @@
 // /app/(dashboard)/layout.js - ONLY for normal dashboard pages
-"use client";
 
 import { AppSidebar } from "@/src/app/components/dashboard/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/src/app/components/ui/sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  // Assume this is only called for real dashboard pages
   return (
     <SidebarProvider className="overflow-hidden">
       <AppSidebar />
--- src/app/components/dashboard/app-sidebar.tsx
+++ src/app/components/dashboard/app-sidebar.tsx
@@ -1,7 +1,5 @@
-"use client";
 
 import * as React from "react";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail } from "@/src/app/components/ui/sidebar";
@@ -9,32 +7,11 @@ import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, Si
 import { getNavItemsByRole } from "@/src/app/lib/navItems.config";
 import DashboardSideBarContent from "./DashboardSideBarContent";
 import UserCard from "./UserCard";
-import { Spinner } from "../ui/spinner";
 import { getUser } from "../../services/auth/getme";
 
-export function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
-  const [userInfo, setUserInfo] = useState<any>(null);
-  const [navItems, setNavItems] = useState<any[]>([]);
-
-  useEffect(() => {
-
-    const fetchData = async () => {
-      const user = await getUser();
-      setUserInfo(user);
-      const nav = getNavItemsByRole(user?.role || []);
-      setNavItems(nav);
-    };
-    fetchData();
-  }, []);
-
-  // Show loading while fetching user info
-  if (!userInfo) {
-    return (
-      <div>
-        <Spinner />
-      </div>
-    );
-  }
+export async function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
+  const userInfo = await getUser();
+  const navItems = getNavItemsByRole( userInfo?.role || []);
 
   return (
     <Sidebar {...props}>
