cb97e1a6bc75a39e4d29ebda6cadb333674c0209
Yeasin <ar9485727@gmail.com>
2025-12-20 21:52:43 +0600

    update the proxy import problem

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import Jwt from "jsonwebtoken";
-import { removeCookie } from "./tokenHandler";
 
 export enum IUserRole {
   USER = "user",
